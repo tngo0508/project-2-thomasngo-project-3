496094be2b4c8712f4d82c3fe61956f904068698
Thomas Ngo <tngo0508@csu.fullerton.edu>
2016-11-05 00:36:50 -0700

    Update ChutesAndLaddersGame.hpp
    implemented until ChutesAndLaddersGame function

--- ChutesAndLaddersGame.hpp
+++ ChutesAndLaddersGame.hpp
@@ -1,51 +1,61 @@
 //
-//  ChutesAndLaddersGame.hpp
+//  ChutesAndLaddersGame.cpp
 //
 
-#pragma once
-
-#include <stdio.h>
+#include <iostream>
 #include <string>
 
-#include "ArrayQueue.h"
-#include "Player.hpp"
+#include "ChutesAndLaddersGame.hpp"
 #include "GameBoard.hpp"
+#include "Player.hpp"
 
 using namespace std;
 
-// TO DO - change the default value to the number of your team members
-const int MIN_NUMBER_OF_PLAYERS = 2;
-
-class ChutesAndLaddersGame {
-public:
-   // TODO: implement the constructor with all your team members
-   // constructor with the default value of 2 minimim players
-   ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
-
-   // TODO: implement the destructor
-   // destructor
-   ~ChutesAndLaddersGame();
-   
-   // accessors
-   // TO DO: implement the accessor to get the number Of Players
-   long getNumberOfPlayers() {
-      // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
-   }
-   string getWinner() { return winner; }
-   
+// TODO: implement the constructor with all your team members
+// constructor with the default value of a minimum players
+ChutesAndLaddersGame::ChutesAndLaddersGame(int nPlayers) : winner("no winner") {
    // TODO: implement this function properly
-   // reset the game - (1) this will reset the position of each player to zero
-   void resetGame();
-   
-   // TODO: implement this function properly
-   // play the Chutes and Ladders game
-   void playGame();
-private:
-   const int WINNING_POSITION = 100;
-   
-   string winner;       // the winner
-   GameBoard gameBoard; // the game board
+   //throw std::logic_error("not implemented yet");
+	Player player1("Thomas");
+	Player player2("William");
+	players.enqueue(player1);
+	players.enqueue(player2);
+}
 
-   // TO DO: use ArrayQueue to store the players
-};
+// TODO: implement the destructor
+// destructor - dequeue players from the queue
+ChutesAndLaddersGame::~ChutesAndLaddersGame() {
+   // TODO: implement this function properly
+   //throw std::logic_error("not implemented yet");
+	for (int i = 0; i < players.size(); i++)
+	{
+		players.dequeue();
+	}
+}
+
+// TO DO: implement this function properly
+// reset the game - rebuild the list of players
+//        (i.e., the list should be the same as in the constructor).
+//        Place all players at the figurative square zero
+void ChutesAndLaddersGame::resetGame() {
+   // TODO: implement this function properly
+   //throw std::logic_error("not implemented yet");
+	players.empty();
+	ChutesAndLaddersGame();
+}
+
+// TO DO: implement this function properly
+// Play the chutes and ladders until a player reaches the winning square 100
+//    - Each player takes turn to roll the die and moves to the new square by invoking rollDieAndMove.
+//         If the new square is outside of the board, the player stays put
+//    - Player's new position is checked against the game board's checkChutesLadders
+//    - checkChutesLadders returns a different square if player lands on a chute or a ladder
+//    - Player's position is then set to the new position as indicated by checkChutesLadders
+//          If player lands on a chute or a ladder, player slides down or climbs up
+//    - If player lands on the winning square 100, game is over
+//    - playGame returns after congratulating and printing the winner's name
+void ChutesAndLaddersGame::playGame() {
+   // TODO: implement this function properly
+   throw std::logic_error("not implemented yet");
+   
+}
