325de045af3467927f4992df4462224775f2b145
Thomas Ngo <tngo0508@csu.fullerton.edu>
2016-11-06 15:51:55 -0800

    Update ChutesAndLaddersGame.hpp
    clean code

--- ChutesAndLaddersGame.hpp
+++ ChutesAndLaddersGame.hpp
@@ -13,41 +13,30 @@
 
 using namespace std;
 
-// TO DO - change the default value to the number of your team members
+
 const int MIN_NUMBER_OF_PLAYERS = 2;
 
 class ChutesAndLaddersGame {
 public:
-   // TODO: implement the constructor with all your team members
-   // constructor with the default value of 2 minimim players
-   ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
+	ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
 
-   // TODO: implement the destructor
-   // destructor
-   ~ChutesAndLaddersGame();
-   
-   // accessors
-   // TO DO: implement the accessor to get the number Of Players
-   int getNumberOfPlayers() {
-      // TODO: implement this function properly
-      //throw std::logic_error("not implemented yet");
-	   return players.size();
-   }
-   string getWinner() { return winner; }
-   
-   // TODO: implement this function properly
-   // reset the game - (1) this will reset the position of each player to zero
-   void resetGame();
-   
-   // TODO: implement this function properly
-   // play the Chutes and Ladders game
-   void playGame();
+	// destructor
+	~ChutesAndLaddersGame();
+
+	// accessors
+	int getNumberOfPlayers() {
+		return MIN_NUMBER_OF_PLAYERS;
+	}
+	string getWinner() { return winner; }
+
+	void resetGame();
+
+	void playGame();
 private:
-   const int WINNING_POSITION = 100;
-   
-   string winner;       // the winner
-   GameBoard gameBoard; // the game board
+	const int WINNING_POSITION = 100;
 
-   // TO DO: use ArrayQueue to store the players
-   ArrayQueue<Player> players;
+	string winner;       // the winner
+	GameBoard gameBoard; // the game board
+
+	ArrayQueue<Player> playerList;
 };
